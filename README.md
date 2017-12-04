ae72936bf2b7c156bf1faf1c99452be063f22576
Stefan van Gastel <stefanvangastel@gmail.com>
2017-12-04 19:51:27 +0100

    Added multi-phone option

--- README.md
+++ README.md
@@ -2,24 +2,29 @@
 This super simple small (selfhosted!) php script shows a given webpage (url) in a iPhone (6) dimension surrounded by an iPhone frame. Many hosted alternatives are out there, though this one you can use on a self-hosted environment and edit to your needs. Planning to add more phone types.
 
 ## Example
-Live example: [here](http://giix.nl/demo/webpage-in-iphone-frame/index.php?url=http://creativiteiten.nl).
+Live example: [here](http://giix.nl/demo/webpage-in-iphone-frame/index.php?URL_1=http://creativiteiten.nl).
 
 Will look like this (50% image size):
 
 ![Example](https://github.com/stefanvangastel/webpage-in-iphone-frame/blob/master/img/example_half.png)
 
+## Config
+
+Every `URL_*` get param or environment variable (Docker) will be added as a phone with the page loaded.
+
+So you can use it like `http://mywebsite/index.php?URL_1=http://example.com&URL_2=http://foo.bar` to show two phones.
+
 ## Install
 You can either run a container or host the php file yourself.
 
 ### Docker
-`docker run -d -p 80:80 -e URL=http://example.com stefanvangastel/webpage-in-iphone-frame`
-
+`docker run -d -p 80:80 -e URL_1=http://creativiteiten.nl stefanvangastel/webpage-in-iphone-frame`
 
 [![stefanvangastel/webpage-in-iphone-frame docker image](http://dockeri.co/image/stefanvangastel/webpage-in-iphone-frame)](https://registry.hub.docker.com/u/stefanvangastel/webpage-in-iphone-frame/)
 
 ### Webserver
 Make sure you have a webserver that can run PHP and place the contents of this repo in the documentroot. Ten access:
-`http://mywebsite/index.php?url=http://example.com`
+`http://mywebsite/index.php?URL_1=http://example.com`
 
 ## Build
 Build the dockerfile yourself by:
--- index.php
+++ index.php
@@ -8,38 +8,42 @@
     <body>
 
         <?php
-            //Get website
-            $url = 'http://example.com';
+            $urls = [];
 
-            //Read from env (Docker)
-            if(getenv('URL')){
-                $url = getenv('URL');
+            $params = array_merge($_ENV,$_GET);
+
+            //Get all env vars starting with URL_
+            foreach($params as $key => $val){
+                if(stristr($key,'URL_')){
+                    $urls[] = $val;
+                }
             }
 
-            //Get from GET query
-            if(!empty($_GET['url'])){
-                $url = $_GET['url'];
-            }
+            $urls = array_unique($urls);  
         ?>
 
         <section id="devices">
+            
+            <?php foreach($urls as $url) { ?>
+                <div class="deviceWrap iphone-6-portrait">
 
-            <div class="deviceWrap iphone-6-portrait">
+                    <div class="device" style="
+                            width: 377px;
+                            height: 647px;
+                            padding-top: 20px;
+                            padding-bottom: 44px;
+                            border:0px;">
 
-            <div class="device" style="
-                    width: 375px;
-                    height: 603px;
-                    padding-top: 20px;
-                    padding-bottom: 44px;">
+                        <div class="flashingTop" style="height: 20px; width: 375px">
+                            <span class="time"><?php echo date('H:i'); ?></span>
+                        </div>
 
-                <div class="flashingTop" style="height: 20px; width: 375px">
-                    <span class="time"><?php echo date('H:i'); ?></span>
+                        <iframe src="<?php echo $url; ?>" id="iphone-6-portrait" style=""></iframe>
+
+                    </div>
                 </div>
+            <?php } ?>
 
-                <iframe src="<?php echo $url; ?>" id="iphone-6-portrait" style=""></iframe>
-
-            </div>
-        </div>
         </section>
     </body>
 </html>
