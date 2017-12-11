2df1c8dc2b8a8f1b21eb749b1e3e879d448a3ba8
Stefan van Gastel <stefanvangastel@gmail.com>
2017-12-11 19:37:10 +0100

    fixed time

--- index.php
+++ index.php
@@ -4,6 +4,28 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
         <link rel="stylesheet" media="screen" href="css/main.css">
+        <style>
+        <?php
+            //Set timezone 
+            date_default_timezone_set('Europe/Amsterdam');
+
+            //Set scale and overwrite it with env or get params
+            $scale = '1.0';
+
+            if( ! empty($_ENV['scale']) ){
+                $scale = $_ENV['scale'];
+            }
+            if( ! empty($_GET['scale']) ){
+                $scale = $_GET['scale'];
+            }
+        ?>
+            body {
+                transform: scale(<?= $scale ?>);
+                transform-origin: 0 0;
+                -moz-transform: scale(<?= $scale ?>);
+                -webkit-transform: scale(<?= $scale ?>);
+            }
+        </style>
     </head>
     <body>
 
@@ -12,19 +34,32 @@
 
             $params = array_merge($_ENV,$_GET);
 
-            //Get all env vars starting with URL_
+            //Get all env and get vars starting with URL_
             foreach($params as $key => $val){
                 if(stristr($key,'URL_')){
+
+                    //TODO: Filter out optional URL_<ID>_TITLE and URL_<ID>_LINK properties
                     $urls[] = $val;
                 }
             }
 
-            $urls = array_unique($urls);  
+            $urls = array_unique($urls);
+            sort($urls);
         ?>
 
         <section id="devices">
+        
+            <?php
+            //Usage message 
+            if(empty($urls)){
+
+                echo "Usage example: ";
+                echo (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                echo "?URL_1=http://nu.nl&URL_2=http://example.com&scale=0.7";
+            }
             
-            <?php foreach($urls as $url) { ?>
+            foreach($urls as $url) { ?>
+
                 <div class="deviceWrap iphone-6-portrait">
 
                     <div class="device" style="
@@ -34,8 +69,8 @@
                             padding-bottom: 44px;
                             border:0px;">
 
-                        <div class="flashingTop" style="height: 20px; width: 375px">
-                            <span class="time"><?php echo date('H:i'); ?></span>
+                        <div class="flashingTop" style="height: 20px; width: 420px">
+                            <span class="time"><strong><?php echo date('H:i'); ?></strong></span>
                         </div>
 
                         <iframe src="<?php echo $url; ?>" id="iphone-6-portrait" style=""></iframe>
